9ee1462d7dec14c14c9218227f359ea59f15831b
Venus-Panda <panda.anghel01@gmail.com>
2024-05-24 16:16:56 +0300

    main

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practivaltest02v6/general/Constants.java
@@ -0,0 +1,5 @@
+package ro.pub.cs.systems.eim.practivaltest02v6.general;
+
+public interface Constants {
+    final public static String WEB_SERVICE_ADDRESS = "https://api.coindesk.com/v1/bpi/currentprice/EUR.json";
+}
---
+++ app/src/main/java/ro/pub/cs/systems/eim/practivaltest02v6/view/PracticalTest02v6MainActivity.java
@@ -0,0 +1,26 @@
+package ro.pub.cs.systems.eim.practivaltest02v6.view;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import ro.pub.cs.systems.eim.practivaltest02v6.R;
+
+public class PracticalTest02v6MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_practical_test02v6_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
